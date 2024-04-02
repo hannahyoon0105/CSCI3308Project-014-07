7ab8db8c97e48aae78324fa85e1956e661240d4d
Hannah Yoon <hannahyoon@engr2-2-67-210-edu.int.colorado.edu>
2024-04-02 15:12:54 -0600

    changing user table schema and adding get endpoint for home

--- ProjectSourceCode/init_data/create.sql
+++ ProjectSourceCode/init_data/create.sql
@@ -1,8 +1,10 @@
+DROP TABLE IF EXISTS users;
 CREATE TABLE users (
     username VARCHAR(50) PRIMARY KEY,
-    password CHAR(60) NOT NULL
+    password CHAR(60) NOT NULL,
+    profile_pic VARCHAR(200) NOT NULL
 );
-
+DROP TABLE IF EXISTS posts;
 CREATE TABLE posts (
     post_id SERIAL PRIMARY KEY,
     author VARCHAR(50) NOT NULL REFERENCES users(username),
@@ -12,7 +14,7 @@ CREATE TABLE posts (
     image_url VARCHAR(200) NOT NULL,
     original_flag BOOLEAN NOT NULL
 );
-
+DROP TABLE IF EXISTS recipe;
 CREATE TABLE recipe (
     recipe_id SERIAL PRIMARY KEY,
     title VARCHAR(100) NOT NULL,
@@ -20,19 +22,19 @@ CREATE TABLE recipe (
     body TEXT NOT NULL,
     date_created TIMESTAMP NOT NULL
 );
-
+DROP TABLE IF EXISTS followers;
 CREATE TABLE followers (
     follower VARCHAR(50) NOT NULL REFERENCES users(username),
     followee VARCHAR(50) NOT NULL REFERENCES users(username),
     PRIMARY KEY (follower, followee)
 );
-
+DROP TABLE IF EXISTS likes;
 CREATE TABLE likes (
     post_id INTEGER NOT NULL REFERENCES posts(post_id),
     username VARCHAR(50) NOT NULL REFERENCES users(username),
     PRIMARY KEY (post_id, username)
 );
-
+DROP TABLE IF EXISTS comments;
 CREATE TABLE comments (
     post_id INTEGER NOT NULL REFERENCES posts(post_id),
     user VARCHAR(50) NOT NULL REFERENCES users(username),
